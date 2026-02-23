aa1f3a56b0127122f9d29d8613d2f68597bdd88b
Youssef-YN <nazihyoussef05@gmail.com>
2026-02-23 09:54:35 +0000

    Dynamic Presentation done

--- src/main/java/nazih/youssef/presentation/Presentation.java
+++ src/main/java/nazih/youssef/presentation/Presentation.java
@@ -1,13 +1,25 @@
 package nazih.youssef.presentation;
 
-import nazih.youssef.dao.DaoImpl;
-import nazih.youssef.metier.MetierImpl;
+import nazih.youssef.dao.IDao;
+import nazih.youssef.metier.IMetier;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.Scanner;
 
 public class Presentation {
     public static void main(String[] args) {
-        DaoImpl dao=new DaoImpl();
-        MetierImpl metier=new MetierImpl();
-        metier.setDao(dao);
-        System.out.println(metier.calcul());
+        try {
+            Scanner scanner=new Scanner(new File("config.txt"));
+            String daoClassname=scanner.next();
+            String metierClassName=scanner.next();
+            Class<?> cdao= Class.forName(daoClassname);
+            IDao dao= (IDao) cdao.newInstance();
+            Class<?> cmetier= Class.forName(metierClassName);
+            IMetier metier=(IMetier) cmetier.newInstance();
+            Method meth=cmetier.getMethod("setDao",IDao.class);
+            meth.invoke(metier,dao);
+            System.out.println(metier.calcul());
+        } catch (Exception e) { e.printStackTrace(); }
     }
 }
